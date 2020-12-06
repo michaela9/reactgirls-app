ec8f2166d6cf4c5e4a3ee632d08e5137b7244c35
michaela9 <58274290+michaela9@users.noreply.github.com>
2020-12-06 16:58:40 +0100

    Update variables.js

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,4 @@
+
 export const sizes = {
     sizeXXS: `12px`,
     sizeXS: `14px`,
@@ -14,6 +15,7 @@ export const colors = {
     lightBlue: `#EBF6F8`,
     darkBlue: `#075C68`,
     lightGrey: `#B6B6B6`,
+    middleGrey: `#D7D7D7`,
     grey: `#9D9D9D`,
     darkGrey: `#323232`,
     white: `#FFFBFA`,
